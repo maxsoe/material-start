213bed09200e435fb20341792eb302266b148f00
Max Soe <maxsoe@gmail.com>
2017-12-17 00:45:38 +0000

    7 - XHR & Dependency Injection

--- angular-tutorial/phone-list/phone-list.component.js
+++ angular-tutorial/phone-list/phone-list.component.js
@@ -1,23 +1,18 @@
 // Register `phoneList` component, along with its associated controller and template
 angular
-.module('phoneList')
-.component('phoneList', {
-  templateUrl: 'phone-list/phone-list.template.html',
-  controller: function PhoneListController() {
-    this.phones = [{
-      name: 'Nexus S',
-      snippet: 'Fast just got faster with Nexus S.',
-      age: 1
-    }, {
-      name: 'Motorola XOOM™ with Wi-Fi',
-      snippet: 'The Next, Next Generation tablet.',
-      age: 2
-    }, {
-      name: 'MOTOROLA XOOM™',
-      snippet: 'The Next, Next Generation tablet.',
-      age: 3
-    }];
+  .module('phoneList')
+  .component('phoneList', {
+    templateUrl: 'phone-list/phone-list.template.html',
+    controller: ['$http',
+      function PhoneListController($http) {
+        var self = this;
+        self.orderProp = 'age';
 
-    this.orderProp = 'age';
-  }
-});
+        $http.get('phones/phones.json').then(function(response) {
+          self.phones = response.data;
+          // pre-process the HTTP response by limiting the number of phones to the first 5 in the list
+          // self.phones = response.data.slice(0,5);
+        })
+      }
+    ]
+  });
